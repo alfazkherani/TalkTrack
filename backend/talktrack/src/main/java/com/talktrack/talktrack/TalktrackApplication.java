cb1af0eaa2a81b1c3c4bd4d4fbe98929d523b9ff
moradiya84 <ommoradiya6p556@gmail.com>
2025-05-30 02:35:19 +0530

    Backend Setup

---
+++ backend/talktrack/src/main/java/com/talktrack/talktrack/TalktrackApplication.java
@@ -0,0 +1,13 @@
+package com.talktrack.talktrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalktrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalktrackApplication.class, args);
+	}
+
+}
